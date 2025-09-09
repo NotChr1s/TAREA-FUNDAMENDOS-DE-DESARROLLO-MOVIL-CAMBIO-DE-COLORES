871ccee52156d70a4ee23b2e7d1910faccefc9b3
NotChr1s <143003134+NotChr1s@users.noreply.github.com>
2025-09-09 14:41:32 -0600

    Second commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "G:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\POLLO\\StudioProjects\\test1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=G:\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\POLLO\\StudioProjects\\test1"
+  "FLUTTER_ROOT=G:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\POLLO\\StudioProjects\\test1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\POLLO\\StudioProjects\\test1"
+  "FLUTTER_TARGET=C:\\Users\\POLLO\\StudioProjects\\test1\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzUuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MDVkYjk2ODkwOA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YThiZmRmYzM5NA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjA="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\POLLO\\StudioProjects\\test1\\.dart_tool\\package_config.json"
+)
